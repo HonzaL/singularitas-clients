77de666aecd4f521fe80752a532a933bd48c5e4a
lisal.j <lisal.j@st-software.com>
2016-05-18 20:14:38 +0200

    Message change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 server.get('/',function (request, response) {
-	response.json({hello: 'World!'});
+	response.json({hello: 'World! - 2'});
 });
 
 server.listen(server_port, server_ip_address, function () {
