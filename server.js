4661d9146bb671f156836a3a145c01328cfce1b9
lisal.j <lisal.j@st-software.com>
2016-05-18 19:39:20 +0200

    Openshift env properties

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ var express = require('express');
 
 var server = express();
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 server.get('/',function (request, response) {
 	response.json({hello: 'World!'});
 });
 
-server.listen(8080, function () {
-  console.log('Server is listening on port 8080!');
+server.listen(server_port, server_ip_address, function () {
+  console.log('Server is listening on ' + server_ip_address + ":" + server_port);
 });
